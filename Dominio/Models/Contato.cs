f9458e070d009e749f10b23d5694f304272546ce
engDaniloOS <36059157+engDaniloOS@users.noreply.github.com>
2019-08-14 17:20:43 -0300

    Exclusão de arquivos desnecessários

--- Dominio/Models/Contato.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Dominio.Models
-{
-    public class Contato : EntidadeBase
-    {
-        public string Email { get; set; }
-        public string Celular { get; set; }
-        public string Telefone { get; set; }
-    }
-}
